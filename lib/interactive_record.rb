1385934abb11a22738288838397e256c12ae32d2
jamieberrier <berrier.jamie@gmail.com>
2019-06-05 19:11:50 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,6 +52,9 @@ class InteractiveRecord
     self.class.column_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
+    #self.class.column_names.each do |col_name|
+    #  values << "'#{send(col_name)}'" unless send(col_name).nil?
+    #end
     values.join(", ")
     binding.pry
   end
