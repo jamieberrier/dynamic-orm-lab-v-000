8636226ca669b236372bc48e53c810e8ef4115d9
jamieberrier <berrier.jamie@gmail.com>
2019-06-05 18:59:23 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -15,7 +15,7 @@ class InteractiveRecord
 
     table_info = DB[:conn].execute(sql)
     column_names = []
-    table_info.each.with_index do |row|
+    table_info.each do |row|
       column_names << row["name"]
     end
     column_names.compact
@@ -38,7 +38,7 @@ class InteractiveRecord
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    
+
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
 
