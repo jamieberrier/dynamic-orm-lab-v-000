5f231628c995089b4a87cec0ca84720869d3cb72
jamieberrier <berrier.jamie@gmail.com>
2019-06-05 18:29:38 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -65,7 +65,7 @@ class InteractiveRecord
     #binding.pry
     attribute.each do |key, value|
       sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
-      DB[:conn].execute(sql)
     end
+    DB[:conn].execute(sql)
   end
 end
