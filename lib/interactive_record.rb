8b0a3024986adba09330d1abec982c1663f3fa0b
jamieberrier <berrier.jamie@gmail.com>
2019-06-05 19:12:08 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -49,7 +49,7 @@ class InteractiveRecord
 
   def values_for_insert
     values = []
-    self.class.column_names.each do |col_name|
+    @c_names.each do |col_name|
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
     #self.class.column_names.each do |col_name|
