1819b5f8511c34503f2e32e4ffbad00e0ac41244
jamieberrier <berrier.jamie@gmail.com>
2019-06-05 18:27:55 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -64,7 +64,7 @@ class InteractiveRecord
   def self.find_by(attribute)
     binding.pry
     attribute.each do |key, value|
-      sql = "SELECT * FROM #{self.table_name} WHERE attribute.key = '#{attribute.value}'"
+      sql = "SELECT * FROM #{self.table_name} WHERE #{key} = '#{value}'"
       DB[:conn].execute(sql)
     end
   end
